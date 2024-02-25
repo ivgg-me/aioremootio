7bf2b4856fe7494d52b2d048e06725504cb366f4
Ilyés-Veisz, G. Gábor <ilyes.gergo@gmail.com>
2024-02-25 11:00:08 +0100

    Bump version: 1.0.0.alpha21 → 1.0.0.dev0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ packages.extend(find_packages(where="src"))
 
 setup(
     name="aioremootio",
-    version="1.0.0.alpha21",
+    version="1.0.0.dev0",
     description="An asynchronous API client library for Remootio (http://www.remootio.com/)",
     long_description=long_description,
     long_description_content_type="text/markdown",
